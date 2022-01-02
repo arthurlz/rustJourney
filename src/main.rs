9bf1608321bb6981d5a9a48a7584712dda72aa8f
lizheng <litar929@gmail.com>
2022-01-02 18:24:21 +0900

    transmute

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,14 @@ fn main() {
     let slice = &mut arr[..];
     slice[0] = 100000;
     println!("arr[0]={}", arr[0]);
+
+    let a: i8 = -10;
+    let b = a as u8;
+    println!("a={} b={}", a, b);
+
+    unsafe {
+        let a = [0u8, 1u8, 0u8, 0u8];
+        let b: u32 = mem::transmute(a);
+        println!("{}", b);
+    }
 }
