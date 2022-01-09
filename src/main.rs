4ac265a5d6fac1b40d5e93dde643f3079bd586e6
lizheng <litar929@gmail.com>
2022-01-09 17:10:59 +0900

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 fn main() {
+    let a: i32 = 18;
+    let b: char = 'A';
+
+    // tuple
+    let t = (a, b);
+
+    // pattern matching
+    let (c, d) = t;
+    println!("c={}, d={}", c, d);
+
+    println!("t.0={}", t.0);
+    println!("t.1={}", t.1);
+    
+    let (result, is_overflow)  = a.overflowing_add(10);
+    println!("result={}, is_overflow={}", result, is_overflow);
+
     // slice
     let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
     let slice = &arr[0..3]; // range
